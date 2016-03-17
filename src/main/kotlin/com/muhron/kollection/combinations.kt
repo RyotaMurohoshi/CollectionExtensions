c1d13941445856e1066584f0db4274df7ead0015
RyotaMurohoshi <ryotamurohoshi@gmail.com>
2016-03-18 00:13:57 +0900

    added extension function

--- src/main/kotlin/com/muhron/kollection/combinations.kt
+++ src/main/kotlin/com/muhron/kollection/combinations.kt
@@ -1,8 +1,18 @@
 package com.muhron.kollection
 
+@JvmName("combinationsExtension")
+fun <A, B> List<A>.combinations(listB: List<B>): List<Pair<A, B>> =
+        combinations<A, B>(this, listB)
+
+@JvmName("combinations")
 fun <A, B> combinations(listA: List<A>, listB: List<B>): List<Pair<A, B>> =
         listA.flatMap { first -> listB.map { second -> first to second } }
 
+@JvmName("combinationsExtension")
+fun <A, B, C> List<A>.combinations(listB: List<B>, listC: List<C>): List<Triple<A, B, C>> =
+        combinations<A, B, C>(this, listB, listC)
+
+@JvmName("combinations")
 fun <A, B, C> combinations(listA: List<A>, listB: List<B>, listC: List<C>): List<Triple<A, B, C>> =
         listA.flatMap { first ->
             listB.flatMap { second ->
