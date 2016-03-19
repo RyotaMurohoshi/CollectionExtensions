f6d8aa976bf1882259c4c692a978c5455f519e75
RyotaMurohoshi <ryotamurohoshi@gmail.com>
2016-03-19 13:43:32 +0900

    added groupJoin

---
+++ src/main/kotlin/com/muhron/kollection/groupJoin.kt
@@ -0,0 +1,25 @@
+package com.muhron.kollection
+
+fun <TOuter, TInner, TKey, TResult> List<TOuter>.groupJoin(
+        inner: List<TInner>,
+        outerKeySelector: (TOuter) -> TKey,
+        innerKeySelector: (TInner) -> TKey,
+        resultSelector: (TOuter, List<TInner>) -> TResult): List<TResult> =
+        groupJoin(this, inner, outerKeySelector, innerKeySelector, resultSelector)
+
+@JvmName("groupJoins")
+fun <TOuter, TInner, TKey, TResult> groupJoin(
+        outer: List<TOuter>,
+        inner: List<TInner>,
+        outerKeySelector: (TOuter) -> TKey,
+        innerKeySelector: (TInner) -> TKey,
+        resultSelector: (TOuter, List<TInner>) -> TResult): List<TResult> {
+
+    val innerLookup = inner.groupBy(innerKeySelector)
+    return outer.map { outerElement ->
+        val key = outerKeySelector(outerElement)
+        val innerElements = innerLookup[key]
+
+        resultSelector(outerElement, innerElements ?: emptyList<TInner>())
+    }
+}
