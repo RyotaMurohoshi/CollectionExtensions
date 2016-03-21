1ded0dcaf3fa9f236337fde9b33e1c85f9a19f23
RyotaMurohoshi <ryotamurohoshi@gmail.com>
2016-03-22 00:08:40 +0900

    added buffer method

---
+++ src/main/kotlin/com/muhron/kollection/buffer.kt
@@ -0,0 +1,35 @@
+package com.muhron.kollection
+
+import java.util.LinkedList
+import java.util.ArrayList
+
+fun <TSource> List<TSource>.buffer(count: Int): List<List<TSource>> = buffer(count, count)
+
+fun <TSource> List<TSource>.buffer(count: Int, skip: Int): List<List<TSource>> {
+
+    val buffers = LinkedList<ArrayList<TSource>>()
+    val result = ArrayList<List<TSource>>()
+
+    var index = 0;
+    for (item in this) {
+        if (index % skip == 0) {
+            buffers.add(ArrayList(count))
+        }
+
+        for (buffer in buffers) {
+            buffer.add(item)
+        }
+
+        if (buffers.size > 0 && buffers.peek().size == count) {
+            result.add(buffers.remove())
+        }
+
+        index++
+    }
+
+    while (buffers.size > 0) {
+        result.add(buffers.remove())
+    }
+
+    return result
+}
